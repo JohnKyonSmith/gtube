8d79149b7a4f4c2fd5e05d95fa223d4150e5c564
FibreChips <douglinning@gmail.com>
2013-11-11 18:34:31 -0600

    Fixed styles, added rotating footer message

--- index.php
+++ index.php
@@ -13,72 +13,72 @@
 
 
 
-    <body>
-	<header>
-            <a href="http://gtube.muhprivacy.com/">gTube</a>
-	    <form id="search">
-	        <input id="searchbar" type="text" name="Keyword" autofocus/>
-	        <input id="searchb" type="submit" value="Search" />
-	    </form>
-	</header>
-	<div id="wrapper">
-		<div class="section">
-			<div class="secTitle">Newest Videos</div>
-				<ul class="videos">
-				<?php
-							foreach (glob("video/*.webm") as $file) {
-								$id = preg_replace('/video\//', "", preg_replace('/\.webm/', "", $file));
-						echo "<li>\n";
-						echo "\t<a href=\"video.php?id=$id\">\n";
-					echo "\t<img src='video/previews/$id.png' class='videoThumb'>";
-					echo "<div class=\"videoTitle\">$id</div>\n";
-						echo "\t</a>\n";
-						echo "</li>\n";
-					}
-						?>
-				</ul>
-			</div>
+<body>
+	<div id="container">
+		<header>
+			<a href="http://gtube.muhprivacy.com/">gTube</a>
+			<form id="search">
+				<input id="searchbar" type="text" name="Keyword" autofocus/>
+				<input id="searchb" type="submit" value="Search" />
+			</form>
+		</header>
+		<div id="wrapper">
 			<div class="section">
-			<div class="secTitle">Top Videos</div>
-				<ul class="videos">
-				<?php
-							foreach (glob("video/*.webm") as $file) {
-								$id = preg_replace('/video\//', "", preg_replace('/\.webm/', "", $file));
-						echo "<li>\n";
-						echo "\t<a href=\"video.php?id=$id\">\n";
-					echo "\t<img src='video/previews/$id.png' class='videoThumb'>";
-					echo "<div class=\"videoTitle\">$id</div>\n";
-						echo "\t</a>\n";
-						echo "</li>\n";
-					}
-						?>
-				</ul>
-			</div>
-		<div class="section">
-			<div class="secTitle">Videos Being Watched</div>
-				<ul class="videos">
-				<?php
-							foreach (glob("video/*.webm") as $file) {
-								$id = preg_replace('/video\//', "", preg_replace('/\.webm/', "", $file));
-						echo "<li>\n";
-						echo "\t<a href=\"video.php?id=$id\">\n";
-					echo "\t<img src='video/previews/$id.png' class='videoThumb'>";
-					echo "<div class=\"videoTitle\">$id</div>\n";
-						echo "\t</a>\n";
-						echo "</li>\n";
-					}
-						?>
-				</ul>
+				<div class="secTitle">Newest Videos</div>
+					<ul class="videos">
+					<?php
+								foreach (glob("video/*.webm") as $file) {
+									$id = preg_replace('/video\//', "", preg_replace('/\.webm/', "", $file));
+							echo "<li>\n";
+							echo "\t<a href=\"video.php?id=$id\">\n";
+						echo "\t<img src='video/previews/$id.png' class='videoThumb'>";
+						echo "<div class=\"videoTitle\">$id</div>\n";
+							echo "\t</a>\n";
+							echo "</li>\n";
+						}
+							?>
+					</ul>
+				</div>
+				<div class="section">
+				<div class="secTitle">Top Videos</div>
+					<ul class="videos">
+					<?php
+								foreach (glob("video/*.webm") as $file) {
+									$id = preg_replace('/video\//', "", preg_replace('/\.webm/', "", $file));
+							echo "<li>\n";
+							echo "\t<a href=\"video.php?id=$id\">\n";
+						echo "\t<img src='video/previews/$id.png' class='videoThumb'>";
+						echo "<div class=\"videoTitle\">$id</div>\n";
+							echo "\t</a>\n";
+							echo "</li>\n";
+						}
+							?>
+					</ul>
+				</div>
+			<div class="section">
+				<div class="secTitle">Videos Being Watched</div>
+					<ul class="videos">
+					<?php
+								foreach (glob("video/*.webm") as $file) {
+									$id = preg_replace('/video\//', "", preg_replace('/\.webm/', "", $file));
+							echo "<li>\n";
+							echo "\t<a href=\"video.php?id=$id\">\n";
+						echo "\t<img src='video/previews/$id.png' class='videoThumb'>";
+						echo "<div class=\"videoTitle\">$id</div>\n";
+							echo "\t</a>\n";
+							echo "</li>\n";
+						}
+							?>
+					</ul>
+				</div>
+		</div>
+			<div id="footer">
+				<p><?php $f_contents = file("footers.txt"); $line = $f_contents[rand(0, count($f_contents) - 1)]; echo($line);?></p>
+				<form id="ulform" enctype="multipart/form-data" action="upload.php" method="post">
+					<input name="vidfile" id="vidfile" type="file" />
+					<input type="submit" value="Upload" name="submit" id="submit" />
+				</form>
 			</div>
 	</div>
-		
-        <form enctype="multipart/form-data" action="upload.php" method="post">
-            <input name="vidfile" id="vidfile" type="file" />
-            <input type="submit" value="Upload" name="submit" id="submit" />
-        </form>
-        <br>
-        <div id="footer">
-            Created for your freedoms.
-        </div>
-    </body>
+</body>
 </html>
